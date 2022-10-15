aa7139fb646fcd58e6ffe0fdabcaee0fbdaa9819
Esteban Kim <Esteban.Kim1988@gmail.com>
2022-10-14 23:47:58 -0500

    final working dice... TODO: change based on new requirements

--- VirtualCandy/Assets/Scripts/DiceText.cs
+++ VirtualCandy/Assets/Scripts/DiceText.cs
@@ -7,16 +7,26 @@ public class DiceText : MonoBehaviour
 {
     Text text;
     public static int diceNumber;
+    public static int prevNumber;
 
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<Text>();
+        prevNumber = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
         text.text = diceNumber.ToString();
+
+        if (prevNumber != diceNumber)
+        {
+            Debug.Log("Current Dice Number: " + diceNumber.ToString());
+        }
+
+        prevNumber = diceNumber;
+
     }
 }
